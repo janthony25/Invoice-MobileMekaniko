8588c9737e08c67430950c87f49beda151b7c678
Jun Anthony <Unknown@DESKTOP-LAR4J4U>
2024-07-02 23:04:05 +1200

    Add Index of CustomerController

--- Invoice-MobileMekaniko/Controllers/CustomerController.cs
+++ Invoice-MobileMekaniko/Controllers/CustomerController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Invoice_MobileMekaniko.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Invoice_MobileMekaniko.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerController(ICustomerRepository customerRepository)
         {
-            return View();
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customerList = await _customerRepository.GetAllCustomersAsync();
+            return View(customerList);
         }
     }
 }
