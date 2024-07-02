58c8050fd4b1613c82d849217091472f19cfa936
Jun Anthony <Unknown@DESKTOP-LAR4J4U>
2024-07-02 22:36:35 +1200

    Add Connectionstring to Program.cs

--- Invoice-MobileMekaniko/Program.cs
+++ Invoice-MobileMekaniko/Program.cs
@@ -1,7 +1,15 @@
+using Invoice_MobileMekaniko.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Connection to Db
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
